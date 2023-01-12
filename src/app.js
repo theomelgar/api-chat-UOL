b3d4e284ff43d7f4b8f20fcce4146643cb3ea040
theomelgar <theomelgar@gmail.com>
2023-01-12 20:23:49 -0300

    feat:mongo/dotenv

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express"
 import cors from "cors"
+import { MongoClient, MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db
+try{
+await mongoClient.connect()
+db = mongoClient.db()
+}catch(err){
+    console.log(err)
+}
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = 5000
-app.listen(PORT, () => "Server online in"+PORT) 
+app.listen(PORT, () => "Server online in" + PORT) 
