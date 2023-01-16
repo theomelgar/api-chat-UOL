1a9f2255cb6e561987a30a6dcb0365d31e396cdf
theomelgar <theomelgar@gmail.com>
2023-01-16 03:31:29 -0300

    feat: add removeParticipants

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db
 try {
     await mongoClient.connect()
-    
+
 } catch (error) {
     console.log("Server Error")
 }
@@ -24,22 +24,28 @@ app.post("/participants", async (req, res) => {
     const participantSchema = joi.object({
         name: joi.string().required(),
     });
-    const validation = participantSchema.validate(participant, {pick:["name"],abortEarly:false})
+    const validation = participantSchema.validate(participant, { pick: ["name"], abortEarly: false })
     console.log(validation)
-    if (validation.error){
+    if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message)
         return res.status(422).send(errors)
     }
     try {
-        const findName = await db.collection("participants").findOne({name:participant.name})
-        if(findName) return res.status(409).send("Já existe com esse nome")
+        const findName = await db.collection("participants").findOne({ name: participant.name })
+        if (findName) return res.status(409).send("Já existe com esse nome")
         const time = dayjs().format("HH:mm:ss")
         await db
             .collection('participants')
-            .insertOne({name:participant.name, lastStatus:entry})
+            .insertOne({ name: participant.name, lastStatus: entry })
         await db
             .collection("messages")
-            .insertOne({from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time})
+            .insertOne({
+                from: participant.name,
+                to: 'Todos',
+                text: 'entra na sala...',
+                type: 'status',
+                time: time
+            })
         res.sendStatus(201)
     } catch (error) {
         console.log(error)
@@ -49,9 +55,9 @@ app.post("/participants", async (req, res) => {
 app.get("/participants", async (req, res) => {
     try {
         const list = await db
-        .collection("participants")
-        .find()
-        .toArray()
+            .collection("participants")
+            .find()
+            .toArray()
         res.send(list)
     } catch (error) {
         console.log(error)
@@ -66,19 +72,19 @@ app.post("/messages", async (req, res) => {
         text: joi.string().required(),
         type: joi.string().required().valid("message", "private_message"),
     });
-    const validation = messageSchema.validate(message, {pick:["to","text","type"],abortEarly:false})
+    const validation = messageSchema.validate(message, { pick: ["to", "text", "type"], abortEarly: false })
     console.log(validation)
-    if (validation.error){
+    if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message)
         return res.status(422).send(errors)
     }
     try {
-        const findName = await db.collection("participants").findOne({name:from})
-        if(!findName) return res.status(422).send("Não existe com esse nome")
+        const findName = await db.collection("participants").findOne({ name: from })
+        if (!findName) return res.status(422).send("Não existe com esse nome")
         const time = dayjs().format("HH:mm:ss")
         await db
             .collection("messages")
-            .insertOne({from:from, to: message.to, text: message.text, type: message.type, time: time})
+            .insertOne({ from: from, to: message.to, text: message.text, type: message.type, time: time })
         res.sendStatus(201)
     } catch (error) {
         console.log(error)
@@ -87,14 +93,14 @@ app.post("/messages", async (req, res) => {
 })
 app.get("/messages", async (req, res) => {
     try {
-        const {limit} = parseInt(req.query.limit)
-        const {user} = req.query.user
+        const { limit } = parseInt(req.query.limit)
+        const { user } = req.query.user
         const list = await db
-        .collection("messages")
-        .find({$or : [{type:"status"},{type:"message"},{to:user},{from:user}]})
-        .toArray()
-        if(limit)res.send(list.slice(-limit))
-        if(limit < 1 || isNaN(limit)) res.sendStatus(422)
+            .collection("messages")
+            .find({ $or: [{ type: "status" }, { type: "message" }, { to: user }, { from: user }] })
+            .toArray()
+        if (limit) res.send(list.slice(-limit))
+        if (limit < 1 || isNaN(limit)) res.sendStatus(422)
         res.send(list)
     } catch (error) {
         console.log(error)
@@ -103,16 +109,16 @@ app.get("/messages", async (req, res) => {
 })
 app.post("/status", async (req, res) => {
     try {
-        const {user} = req.headers
+        const { user } = req.headers
         const list = await db
-        .collection("participants")
-        .find({name:user})
-        .toArray()
+            .collection("participants")
+            .find({ name: user })
+            .toArray()
         const entry = Date.now()
         await db
             .collection('participants')
-            .udateOne({lastStatus:entry})
-        if(!list) res.status(404)
+            .udateOne({ name: user }, { $set: { lastStatus: entry } })
+        if (!list) res.status(404)
         res.sendStatus(200)
     } catch (error) {
         console.log(error)
@@ -120,6 +126,36 @@ app.post("/status", async (req, res) => {
     }
 })
 
+async function removeParticipants() {
+    const entry = Date.now()
+    const limit = 10000
+    const time = dayjs().format("HH:mm:ss")
+    try {
+        const list = await db.collection("participants").find().toArray()
+        list.map(async (participant) => {
+            if (participant.lastStatus + limit < entry) {
+                await db
+                    .collection("participants")
+                    .deleteOne(participant)
+
+                await db
+                    .collection("messages")
+                    .insertOne({
+                        from: participant.name,
+                        to: 'Todos',
+                        text: 'saiu na sala...',
+                        type: 'status',
+                        time: time
+                    })
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
+
+setInterval(removeParticipants, 15000);
 const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log("Server online in "+PORT)) 
+app.listen(PORT, () => console.log("Server online in " + PORT)) 
